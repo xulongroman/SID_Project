9605cee368e47c602abab472242ce7f26e1f21f3
XU Long <LX3@dsone.3ds.com>
2019-01-30 08:47:45 +0800

    SIDCmd : time_cmd.log

--- StartCmd.bat
+++ StartCmd.bat
@@ -14,4 +14,4 @@ SET RADF_BUILD_VER=2700
 SET PATH=%A3DT%\%ARCH_RELEASE%\code\bin;%X3DT%\%ARCH_RELEASE%\code\bin;%HOOPS%\bin\%HOOPS_ARCH%;%V8_HOME%\bin\x64;%path%;
 
 cd %AGM_HOME%
-start SIDCmd.exe -file "D:\SINUS-3D\StagingEnvironment\JS-data\AJX051950P2.csv"
+start SIDCmd.exe -file "D:\SINUS-3D\StagingEnvironment\JS-data\nla050006.csv"
