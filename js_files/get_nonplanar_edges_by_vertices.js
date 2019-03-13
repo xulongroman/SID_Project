5321fe2237d30887cd6120c77e1a4242ca58cd26
XU Long <LX3@dsone.3ds.com>
2019-03-13 10:35:13 +0800

    bugfix : pick edge\pick face\pick vertex

--- js_files/get_nonplanar_edges_by_vertices.js
+++ js_files/get_nonplanar_edges_by_vertices.js
@@ -12,9 +12,8 @@ var wirebody = AGM.Object();
 		AGM.Sketcher.CreateLine( 5, 5, 5, -5 ); 
 		AGM.Sketcher.CreateLine( 5, -5, -5, -5 ); 
 	AGM.Sketcher.CloseSketch();
-//	AGM.Sketcher.ConvertToWire(newDoGSketch1, wirebody);
+	AGM.Sketcher.ConvertToWire(newDoGSketch1, wirebody);
 	
-	/*
 	var profile = AGM.Object();
 	AGM.Modelling.CoverPlanarWire(wirebody, profile);
 	var path = AGM.Object();
@@ -26,8 +25,7 @@ var wirebody = AGM.Object();
 	var v3 = AGM.Modelling.PickVertex(AGM.Position(0, 0, 0), AGM.Vector( -1, 1, 0), 10e-4);
 	var v4 = AGM.Modelling.PickVertex(AGM.Position(0, 0, 0), AGM.Vector( -1, -1, 0), 10e-4);
 	
-	var f = AGM.Modelling.PickFace(AGM.Position(0, 0, 1), AGM.Vector(0, 0, -1), 10e-4);
+	var f = AGM.Modelling.PickFace(AGM.Position(0, 0, -1), AGM.Vector(0, 0, 1), 10e-4);
 	var edges = AGM.Modelling.GetNonPlanarEdgesByVertices([v1,v2,v3,v4], f);
 	AGM.Modelling.BlendEdge(edges, 2);
-	*/
 }
