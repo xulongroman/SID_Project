8eecb8fb775a43f1423ddc287e37c9e656f69238
xulongroman <45650834+xulongroman@users.noreply.github.com>
2020-03-18 00:31:45 -0600

    Change the parameter of logpath in AGM.Modelling.ImportFile
    logpath = null means no log;
    logpath = "." means to generate log in current path of the imported file
    logpath = "XXX\YYY.log" means to generate log in the specified path

--- js_files/Import.js
+++ js_files/Import.js
@@ -1,4 +1,4 @@
-﻿AGM.Modelling.ImportFile("D:\\测试数据\\CAM\\nla050006.stp");
+﻿AGM.Modelling.ImportFile("D:\\测试数据\\CAM\\nla050006.stp", "D:\\测试数据\\123.log");
 AGM.Modelling.SaveSat("D:\\测试数据\\CAM\\nla050006.stp.sat", 16);
 	
 	
